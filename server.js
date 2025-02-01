f10277a5c01bffdf95e783cc8ccd4607458103c7
TheDougg <douglas123@live.com.pt>
2025-02-01 19:08:47 -0300

    teste

--- server.js
+++ server.js
@@ -1,49 +1,57 @@
-import express from 'express'
+import express from "express";
+import { PrismaClient } from "@prisma/client";
 
-const app = express()
-app.use(express.json())
-const users = []
+const app = express();
+app.use(express.json());
 
-const checkAuthorization = (req, res, next) => {
-    const token = req.headers['authorization'];
+const prisma = new PrismaClient();
 
-    if (!token || token !== 'Bearer') {
-        return res.status(403).json({ message: 'Acesso negado. Token inválido.' });
-    }
+app.get("/usuarios", async (req, res) => {
+  const users = await prisma.user.findMany();
+  res.status(200).json(users);
+});
 
-    next();
-};
+app.post("/usuarios", async (req, res) => {
+  if (!req.body.email || !req.body.nome || req.body.ativo === undefined) {
+    return res.status(400).json({ error: "Há campos não preenchidos" });
+  }
+  try {
+    await prisma.user.create({
+      data: {
+        email: req.body.email,
+        nome: req.body.nome,
+        ativo: req.body.ativo,
+        disciplinas: req.body.disciplinas,
+      },
+    });
+    res.status(201).json(req.body);
+  } catch (error) {
+    res.status(500).json({ error: "Erro ao criar usuário.", details: error.message });
+  }
+});
 
-app.get('/usuarios', (req, res) => {
-    res.json(users)
-})
+app.put("/usuarios/:id", async (req, res) => {
+  await prisma.user.update({
+    where: {
+      id: req.params.id,
+    },
+    data: {
+      email: req.body.email,
+      nome: req.body.nome,
+      ativo: req.body.ativo,
+      disciplinas: req.body.disciplinas,
+    },
+  });
+  res.status(201).json(req.body);
+});
 
+app.delete("/usuarios/:id", async (req, res) => {
+  await prisma.user.delete({
+    where: {
+      id: req.params.id,
+    },
+  });
+  res.status(200).json({message: "Usuário deletado com sucesso!"})
+});
 
-app.post('/usuarios', checkAuthorization, (req, res) => {
-    const { nome, tipoUsuario, email, materias } = req.body;
-
-    if (!nome || !tipoUsuario || !email || !Array.isArray(materias)) {
-        return res.status(400).json({ message: 'Campos obrigatórios estão faltando ou inválidos.' });
-    }
-
-    if (tipoUsuario !== 'adm' && tipoUsuario !== 'professor') {
-        return res.status(400).json({ message: 'Tipo de usuário inválido.' });
-    }const newUser = {
-        disciplinas
-    };
-
-    users.push(newUser);
-    res.status(201).json(newUser);  // Retorna o usuário criado
-})
-
-
-app.put('/usuarios', (req, res) => {
-    res.send('Requisição de alteração ok')
-})
-
-
-app.delete('/usuarios', (req, res) => {
-    res.send('Requisição de deleção ok')
-})
-
-app.listen(3000)
+app.listen(3000);
